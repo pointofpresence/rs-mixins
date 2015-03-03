5860abe52ac7efbdb4ffd631ffab8e4034dd577d
pointofpresence <github@pointofpresence.ru>
2015-03-03 20:10:31 +0600

    v0.2

--- rs-mixins.js
+++ rs-mixins.js
@@ -1,20 +1,87 @@
 /**
- * rs-mixins v0.1 by point.of.presence
+ * rs-mixins v0.2 by point.of.presence
  * http://pointofpresence.ru
  * https://github.com/pointofpresence/rs-mixins
  */
 
 _.mixin({
     /**
-     It's used to take a string of comma separated names and turn it into an `Array`
-     of names. If an array of names is passed in, it's left as is. Example usage:
+     * Returns the 24-bit color value corresponding to the red, green, blue
+     * components.
+     *
+     * @param red
+     * @param green
+     * @param blue
+     * @returns {number}
+     */
+    rgb: function (red, green, blue) {
+        return blue << 16 | green << 8 | red;
+    },
 
-     _.normalizeArg("Birds, Insects, Flowers");
-     // returns ["Birds", "Insects", "Flowers"]
+    /**
+     * Returns the 32-bit color value corresponding to the red, green, blue and
+     * alpha values.
+     *
+     * @param red
+     * @param green
+     * @param blue
+     * @param alpha
+     * @returns {number}
+     */
+    rgba: function (red, green, blue, alpha) {
+        return alpha << 24 | blue << 16 | green << 8 | red
+    },
 
-     @method normalizeArg
-     @param {String|Array} arg - Either a comma separated string or an array
-     @return {Array} array of normalized names
+    /**
+     * Returns the red component of a color value.
+     *
+     * @param color
+     * @returns {number}
+     */
+    red: function (color) {
+        return color & 255;
+    },
+
+    /**
+     * Returns the green component of a color value.
+     *
+     * @param color
+     * @returns {number}
+     */
+    green: function (color) {
+        return color >> 8 & 255;
+    },
+
+    /**
+     * Returns the blue component of a color value.
+     *
+     * @param color
+     * @returns {number}
+     */
+    blue: function (color) {
+        return color >> 16 & 255;
+    },
+
+    /**
+     * Returns the alpha component of a color value.
+     *
+     * @param color
+     * @returns {number}
+     */
+    alpha: function (color) {
+        return color >> 24 & 255;
+    },
+
+    /**
+     * It's used to take a string of comma separated names and turn it into an
+     * `Array` of names. If an array of names is passed in, it's left as is.
+     * Example usage:
+     *
+     * _.normalizeArg("Birds, Insects, Flowers");
+     * // returns ["Birds", "Insects", "Flowers"]
+     *
+     * @param {String|Array} arg - Either a comma separated string or an array
+     * @return {Array} array of normalized names
      */
     normalizeArg: function (arg) {
         if (_.isString(arg)) {
@@ -29,12 +96,11 @@ _.mixin({
     },
 
     /**
-     Removes a property from an object and returns it if it exists
-
-     @method popProperty
-     @param {Object} obj
-     @param {String} property - property to pop off the object
-     @return {*} popped property
+     * Removes a property from an object and returns it if it exists
+     *
+     * @param {Object} obj
+     * @param {String} property - property to pop off the object
+     * @return {*} popped property
      */
     popProperty: function (obj, property) {
         var val = obj[property];
@@ -43,22 +109,20 @@ _.mixin({
     },
 
     /**
-     Helper method to return a name without an extension
-
-     @method removeExtension
-     @param {String} filename
-     @return {String} filename without an extension
+     * Helper method to return a name without an extension
+     *
+     * @param {String} filename
+     * @return {String} filename without an extension
      */
     removeExtension: function (filename) {
         return filename.replace(/\.(\w{3,4})$/, "");
     },
 
     /**
-     Return the file extension of a filename
-
-     @method fileExtension
-     @param {String} filename
-     @return {String} lowercased extension
+     * Return the file extension of a filename
+     *
+     * @param {String} filename
+     * @return {String} lowercased extension
      */
     fileExtension: function (filename) {
         var fileParts = filename.split(".");
